3c439c8781b9b64e729a4c6b0c53f5003bca4232
Lyndsey Boucherle <lboucherle@gmail.com>
2015-10-27 19:04:27 +0800

    Homework 1 submission

---
+++ Homework1/Homework1/ThirdViewController.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+  /*
+  TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
+  
+  */
+}
---
+++ Homework1/SecondViewController.swift
@@ -0,0 +1,7 @@
+
+import UIKit
+
+class SecondViewController: UIViewController {
+  
+  //TODO five: Display the cumulative sum of all numbers added every time the ‘add’ button is pressed. Hook up the label, text box and button to make this work.
+}
